06111164c1e5fbe494e3bbab6ed974ea55bfd61c
DishonJordan <dishonjordan@hotmail.com>
2019-05-25 15:22:01 -0700

    changed speed settings

--- stacker/stacker/main.c
+++ stacker/stacker/main.c
@@ -44,7 +44,7 @@ void TimerSet(unsigned long M) {
 #define MAX_LEVEL 32
 
 unsigned char grid[MAX_LEVEL];
-unsigned char speeds[MAX_LEVEL] = {20,20,19,19,18,18,17,17,16,16,15,15,14,14,13,13,12,12,11,11,10,10,9,9,8,8,7,7,6,6,5,4}; //In terms of a 10ms Period
+unsigned char speeds[MAX_LEVEL] = {10,10,10,10,10,11,11,11,11,9,9,9,9,8,8,8,8,7,7,7,7,6,6,6,6,6,6,5,5,5,5,4}; //In terms of a 10ms Period
 unsigned char sizes[MAX_LEVEL] = {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1};
 unsigned char row_start[3] = {0x08,0x18,0x38};
 unsigned char level, curr_size, curr_speed, curr_row, button, reset, move,cnt;
